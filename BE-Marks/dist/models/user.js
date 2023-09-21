ef5f4b4f74dd1a19ebf750572a84c3a20b5d671d
DxVxDOD <orbangdavid@yahoo.com>
2023-09-21 19:20:05 +0300

    deploy

---
+++ BE-Marks/dist/models/user.js
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import config from "../utils/config.js";
+import uniqueValidator from "mongoose-unique-validator";
+dotenv.config();
+const { MONGO_URI } = config;
+mongoose.set("strictQuery", false);
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+    console.log("connected to MongoDB");
+})
+    .catch((err) => {
+    console.log("error connecting to MongoDB", err.message);
+});
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: String,
+    passwordHash: String,
+    marks: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Mark",
+        },
+    ],
+});
+userSchema.plugin(uniqueValidator);
+userSchema.set("toJSON", {
+    transform(document, returnedObject) {
+        // Object.assign(returnedObject, {id: returnedObject._id.toString()});
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+        delete returnedObject.password;
+    },
+});
+const User = mongoose.model("User", userSchema);
+export default User;
+//# sourceMappingURL=user.js.map
---
+++ BE-Marks/dist/types/mostLikes.type.js
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=mostLikes.type.js.map
