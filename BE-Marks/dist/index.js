1e03401a0dbd3e42e2717ae3f04e3cfa5cf1c8e0
DxVxDOD <orbangdavid@yahoo.com>
2023-09-21 20:13:42 +0300

    dep2

---
+++ BE-Marks/dist/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import config from "./utils/config.js";
+import logger from "./utils/logger.js";
+import app from "./app.js";
+const PORT = config.PORT;
+app.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
