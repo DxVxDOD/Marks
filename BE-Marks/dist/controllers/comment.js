4bb1a42e4a8be18bc2b63907f0edaaf1ddd1e92d
DxVxDOD <orbangdavid@yahoo.com>
2023-09-21 20:13:42 +0300

    dep2

---
+++ BE-Marks/dist/controllers/comment.js
@@ -0,0 +1,22 @@
+import express from "express";
+import Comment from "../models/comments.js";
+const commentRouter = express.Router();
+commentRouter.get("/", async (request, response) => {
+    const comments = await Comment.find({});
+    response.json(comments);
+});
+commentRouter.post("/", async (request, response) => {
+    if (request.body === undefined) {
+        return response.status(400).json({ error: "Content is missing" });
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { content, markId } = request.body;
+    const comment = new Comment({
+        content,
+        markId,
+    });
+    const savedComment = await comment.save();
+    response.status(201).json(savedComment);
+});
+export default commentRouter;
+//# sourceMappingURL=comment.js.map
