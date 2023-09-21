e3fd420fa0a27d968d401e323bfbed981b4cb99d
DxVxDOD <orbangdavid@yahoo.com>
2023-09-21 18:45:52 +0300

    deploying

---
+++ FE-Marks/dist/models/comments.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import config from "../utils/config.js";
+dotenv.config();
+const { MONGO_URI } = config;
+mongoose.set("strictQuery", false);
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+    console.log("connected to MongoDB");
+})
+    .catch((err) => {
+    console.log("error connecting to MongoDB", err.message);
+});
+const commentSchema = new mongoose.Schema({
+    content: String,
+    markId: String,
+});
+commentSchema.set("toJSON", {
+    transform(document, returnedObject) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
+});
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
+//# sourceMappingURL=comments.js.map
