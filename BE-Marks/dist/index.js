32553604608897d40f1bcfc6be9bfe37d4c67e9c
DxVxDOD <orbangdavid@yahoo.com>
2023-11-19 20:19:47 +0200

    uibuild

--- BE-Marks/dist/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import config from "./utils/config.js";
-import logger from "./utils/logger.js";
-import app from "./app.js";
-const PORT = config.PORT;
-app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`);
-});
-//# sourceMappingURL=index.js.map
