f153cbfaae1a6271120308c043a73057792b1586
David Orbang <orbangdavid@yahoo.com>
2024-01-19 05:24:23 +0200

    resolved user_extractor problem

--- BE_Marks/src/utils/promiseWrapper.ts
+++ BE_Marks/src/utils/promiseWrapper.ts
@@ -1,6 +1,14 @@
+import logger from "./logger";
+
 export const wrapInPromise = async <T>(func: T) => {
   const [result] = await Promise.allSettled([func]);
-  if (result.status === "fulfilled")
-    return { data: result.value, error: undefined };
-  return { data: undefined, error: result.reason };
+  if (result.status === "rejected") {
+    logger.error(result.reason);
+    return { data: undefined, error: result.reason };
+  }
+  if (result.value instanceof Error) {
+    logger.error(result.value);
+    return { data: undefined, error: result.value };
+  }
+  return { data: result.value, error: undefined };
 };
