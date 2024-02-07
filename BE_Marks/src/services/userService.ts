3d28329ef6642bab593f87fc86b39ab827df99c1
David Orbang <orbangdavid@yahoo.com>
2024-02-07 22:14:17 +0000

    resolved promiseWrapper error in error message

--- BE_Marks/src/services/userService.ts
+++ BE_Marks/src/services/userService.ts
@@ -6,52 +6,55 @@ import { newUserParser } from "../utils/parsers/userParser";
 import { wrapInPromise } from "../utils/promiseWrapper";
 
 export const getAllUsers = async () => {
-  const { data: allUserData, error: allUserError } = await wrapInPromise(
-    User.find({}),
-  );
+	const { data: allUserData, error: allUserError } = await wrapInPromise(
+		User.find({})
+	);
 
-  if (allUserError || !allUserData) {
-    throw new Error("Error while fetching all users: " + allUserData);
-  }
+	if (allUserError || !allUserData) {
+		throw new Error("Error while fetching all users: " + allUserData);
+	}
 
-  return allUserData;
+	return allUserData;
 };
 
 export const postNewUser = async (obj: Partial<TNewUser>) => {
-  const { data: allUsersData, error: allUsersError } =
-    await wrapInPromise(getAllUsers());
+	const { data: allUsersData, error: allUsersError } = await wrapInPromise(
+		getAllUsers()
+	);
 
-  if (allUsersError || !allUsersData) {
-    throw new Error("Error while fetching all users: " + allUsersError);
-  }
+	if (allUsersError || !allUsersData) {
+		throw new Error("Error while fetching all users: " + allUsersError);
+	}
 
-  const { data: userData, error: userError } = await wrapInPromise(
-    newUserParser(obj, allUsersData),
-  );
+	const { data: userData, error: userError } = await wrapInPromise(
+		newUserParser(obj, allUsersData)
+	);
 
-  if (userError || !userData) {
-    throw Error(userError);
-  }
+	if (userError || !userData) {
+		return userError;
+	}
 
-  const { data: passwordHashed, error: passwordHashedError } =
-    await wrapInPromise(bcrypt.hash(await stringParser(obj.password), 10));
+	const { data: passwordHashed, error: passwordHashedError } =
+		await wrapInPromise(bcrypt.hash(await stringParser(obj.password), 10));
 
-  if (passwordHashedError || !passwordHashed) {
-    throw new Error("Error while hashing password: " + passwordHashedError);
-  }
+	if (passwordHashedError || !passwordHashed) {
+		throw new Error("Error while hashing password: " + passwordHashedError);
+	}
 
-  const user = new User({
-    ...userData,
-    password: passwordHashed,
-  });
+	const user = new User({
+		...userData,
+		password: passwordHashed,
+	});
 
-  const { data: savedUser, error: savedUserError } = await wrapInPromise(
-    user.save(),
-  );
+	const { data: savedUser, error: savedUserError } = await wrapInPromise(
+		user.save()
+	);
 
-  if (!savedUser || savedUserError) {
-    throw new Error("Error while saving user to database: " + savedUserError);
-  }
+	if (!savedUser || savedUserError) {
+		throw new Error(
+			"Error while saving user to database: " + savedUserError
+		);
+	}
 
-  return savedUser;
+	return savedUser;
 };
--- BE_Marks/src/utils/parsers/markParser.ts
+++ BE_Marks/src/utils/parsers/markParser.ts
@@ -1,34 +1,32 @@
 import { TMarkFE, TNewMark } from "../../types/mark";
-import { wrapInPromise } from "../promiseWrapper";
 import { isMarkFromFE, isNewMark } from "../typeGuards/markGuards";
 import { numberParser, stringParser } from "./generalParsers";
 
-export const newMarkParser = async (obj: Partial<TNewMark>) => {
-	const checkMark = await wrapInPromise(isNewMark(obj));
+export const newMarkParser = (obj: Partial<TNewMark>) => {
+	if (isNewMark(obj)) {
+		const newMark: TNewMark = {
+			tag: stringParser(obj.tag),
+			url: stringParser(obj.url),
+			title: stringParser(obj.title),
+		};
 
-	if (checkMark.error || !checkMark.data) {
-		throw new Error(checkMark.error);
+		return newMark;
 	}
 
-	return {
-		tag: await stringParser(obj.tag),
-		url: await stringParser(obj.url),
-		title: await stringParser(obj.title),
-	};
+	throw new Error("Failed parsing new Mark");
 };
 
-export const markParser = async (obj: Partial<TMarkFE>) => {
-	const checkMark = await wrapInPromise(isMarkFromFE(obj));
-	if (checkMark.error || !checkMark.data) {
-		throw Error(checkMark.error);
+export const markParser = (obj: Partial<TMarkFE>) => {
+	if (isMarkFromFE(obj)) {
+		const mark: TMarkFE = {
+			title: stringParser(obj.title),
+			tag: stringParser(obj.tag),
+			url: stringParser(obj.url),
+			likes: numberParser(obj.likes),
+		};
+
+		return mark;
 	}
 
-	const mark: TMarkFE = {
-		title: await stringParser(obj.title),
-		tag: await stringParser(obj.tag),
-		url: await stringParser(obj.url),
-		likes: await numberParser(obj.likes),
-	};
-
-	return mark;
+	throw new Error("Failed parsing Mark");
 };
