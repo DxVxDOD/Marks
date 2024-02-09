c18fcd48736b23817b9529e1767b99e793680e83
David Orbang <orbangdavid@yahoo.com>
2024-02-09 18:02:44 +0000

    implementing RTKQuery

--- FE-Marks/src/redux/marksBaseApi.ts
+++ FE-Marks/src/redux/marksBaseApi.ts
@@ -3,5 +3,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const marksApi = createApi({
   reducerPath: "marksApi",
   baseQuery: fetchBaseQuery({ baseUrl: "/api/" }),
+  tagTypes: ["Comment", "Mark"],
   endpoints: () => ({}),
 });
--- FE-Marks/src/redux/store.ts
+++ FE-Marks/src/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import notifications from "./slices/notifications";
+import { marksApi } from "./marksBaseApi";
 
 export const store = configureStore({
   reducer: {
     notifications,
+    [marksApi.reducerPath]: marksApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(marksApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
