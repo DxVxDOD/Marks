29d787844366b288bb5b616dc71838bd659c8620
David Orbang <orbangdavid@yahoo.com>
2024-02-09 01:10:56 +0000

    commentSLice

--- FE-Marks/src/redux/slices/comment.ts
+++ FE-Marks/src/redux/slices/comment.ts
@@ -1,1 +1,19 @@
-const baseUrl = "";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { TNewComment } from "../../types/comment";
+
+const baseUrl = "/api/comments";
+
+const initialState: TNewComment[] = [];
+
+const commentSlice = createSlice({
+  name: "comment",
+  initialState,
+  reducers: {
+    set(state, action: PayloadAction<TNewComment[]>) {
+      state = action.payload;
+    },
+    appendComment(state, action: PayloadAction<TNewComment>) {
+      state.push(action.payload);
+    },
+  },
+});
