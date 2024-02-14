9e63a9e46e96a2af811aacfbac712364389bbe18
David Orbang <orbangdavid@yahoo.com>
2024-02-14 18:33:43 +0000

    eliminating old redux code

--- FE-Marks/src/main.tsx
+++ FE-Marks/src/main.tsx
@@ -2,19 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import store, { persist } from "./app/store.ts";
 import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persist}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
