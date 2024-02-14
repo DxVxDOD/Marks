13a8a69cb33bb11ab7c846aaf027b7b5378ec406
David Orbang <orbangdavid@yahoo.com>
2024-02-14 14:49:02 +0000

    comment route done

--- BE_Marks/src/types/user.ts
+++ BE_Marks/src/types/user.ts
@@ -2,8 +2,6 @@ import User from "../models/userModel";
 
 const user = new User();
 
-export type TUser = typeof user & {
-	id: string;
-};
+export type TUser = typeof user;
 
 export type TNewUser = Pick<TUser, "username" | "name" | "password" | "email">;
