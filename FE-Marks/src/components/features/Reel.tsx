be913ae261b6a74f606f5ae34d4e6931eddc861d
David Orbang <orbangdavid@yahoo.com>
2024-02-21 22:00:07 +0000

    reel working properly

--- FE-Marks/src/components/features/Reel.tsx
+++ FE-Marks/src/components/features/Reel.tsx
@@ -1,17 +1,32 @@
-import { Button, Paper, Typography } from "@mui/material";
+import {
+  Paper,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from "@mui/material";
 import { useGetAllMarksQuery } from "../../redux/endpoints/marks";
 import styles from "./reel.module.css";
 import reel from "../../theme/Reel";
 import { useAuth } from "../../hooks/useAuth";
-import { useAppDispatch, useAppSelector } from "../../redux/hook";
+import { useAppDispatch } from "../../redux/hook";
 import { setTag } from "../../redux/slices/filterTag";
+import React, { useRef, useState } from "react";
 
 function Reel() {
   const { data: marks, isFetching } = useGetAllMarksQuery();
   const { classes } = reel();
   const { user } = useAuth();
-  const tag = useAppSelector((state) => state.filterTag);
   const dispatch = useAppDispatch();
+  const textRef = useRef<HTMLSpanElement>(null);
+  const buttonContainerRef = useRef<HTMLDivElement>(null);
+  const [alignment, setAlignment] = useState<string>("all");
+
+  const handleAlignment = (
+    _e: React.MouseEvent<HTMLElement>,
+    newAlignment: string,
+  ) => {
+    setAlignment(newAlignment);
+  };
 
   if (marks && user) {
     if (
@@ -19,6 +34,7 @@ function Reel() {
     ) {
       return (
         <Paper
+          ref={buttonContainerRef}
           sx={{
             border: "solid 1.5px rgba(168, 239, 255, 0.4)",
             width: "75%",
@@ -29,24 +45,35 @@ function Reel() {
             isFetching ? "box " : "" + styles.reel + " " + classes.tagWidth
           }
         >
-          <Button onClick={() => dispatch(setTag("all"))}>
-            <Typography>All</Typography>
-          </Button>
-          {[...marks]
-            .filter((mark) => mark.user.username === user.username)
-            .filter((mark) => {
-              if (tag === "all") {
-                return mark;
-              }
-              return mark.tag === tag;
-            })
-            .map((mark) => (
-              <Button onClick={() => dispatch(setTag(mark.tag))} key={mark.id}>
-                <Typography className={isFetching ? "fetching " : ""}>
-                  {mark.tag}
-                </Typography>
-              </Button>
-            ))}
+          <ToggleButtonGroup
+            exclusive
+            color="info"
+            value={alignment}
+            aria-label="tag button group"
+            onChange={handleAlignment}
+          >
+            <ToggleButton onClick={() => dispatch(setTag("all"))} value="all">
+              <Typography>All</Typography>
+            </ToggleButton>
+            {[...marks]
+              .filter((mark) => mark.user.username === user.username)
+              .map((mark) => mark.tag)
+              .filter((value, index, self) => self.indexOf(value) === index)
+              .map((tag) => (
+                <ToggleButton
+                  value={tag}
+                  key={tag}
+                  onClick={() => dispatch(setTag(tag))}
+                >
+                  <Typography
+                    ref={textRef}
+                    className={isFetching ? "fetching " : ""}
+                  >
+                    {tag}
+                  </Typography>
+                </ToggleButton>
+              ))}
+          </ToggleButtonGroup>
         </Paper>
       );
     }
