7e7cac6cad89b00a996633c33410b1d1f336fb67
DxVxDOD <orbangdavid@yahoo.com>
2024-10-31 21:51:39 +0000

    db database

--- backend/app/db/database.go
+++ backend/app/db/database.go
@@ -1,10 +1,14 @@
 package db
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"log/slog"
+	"marks/app/config"
 	"os"
 
+	"github.com/golang-migrate/migrate/v4"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -28,5 +32,62 @@ func load_config_from_url() (*pgxpool.Config, error) {
 }
 
 func load_config() (*pgxpool.Config, error) {
-	cfg, err := 
+	cfg, err := config.NewDatabase()
+	if err != nil {
+		return load_config_from_url()
+	}
+
+	return pgxpool.ParseConfig(fmt.Sprintf(
+		"user=%s password=%s host=%s port=%d db_name=%s ssl_mode=%s",
+		cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DB_Name, cfg.SSL_Mode,
+	))
+}
+
+func db_url() (string, error) {
+	cfg, err := config.NewDatabase()
+	if err != nil {
+		db_url, ok := os.LookupEnv("DB_URL")
+		if !ok {
+			return "", fmt.Errorf("must set DB_URL env var")
+		}
+
+		return db_url, nil
+	}
+
+	return cfg.URL(), nil
+}
+
+func Connect(ctx context.Context, logger *slog.Logger) (*pgxpool.Pool, error) {
+	config, err := load_config()
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to database: %w", err)
+	}
+
+	logger.Debug("Running migrations")
+
+	migrations_url, exists := os.LookupEnv("MIGRATIONS_URL")
+	if !exists {
+		migrations_url = "file://migrations"
+	}
+
+	url, err := db_url()
+	if err != nil {
+		return nil, err
+	}
+
+	migrator, err := migrate.New(migrations_url, url)
+	if err != nil {
+		return nil, fmt.Errorf("migrate new: %s", err)
+	}
+
+	if err := migrator.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return nil, fmt.Errorf("failed to migrate database: %w", err)
+	}
+
+	return conn, nil
 }
