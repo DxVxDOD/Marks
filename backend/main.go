f49e35e559049cf4aa1d7b642d5c0423638d781e
DxVxDOD <orbangdavid@yahoo.com>
2024-11-01 07:54:46 +0000

    main init

--- backend/main.go
+++ backend/main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"context"
 	"log/slog"
+	"marks/app"
 	"os"
 	"os/signal"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
+
+	a := app.New(logger)
+
+	if err := a.Start(ctx); err != nil {
+		logger.Error("failed to start server", slog.Any("error", err))
+	}
 
 }
