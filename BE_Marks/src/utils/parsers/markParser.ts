1bcf6db66f1f57e03e2ee2f5c11e62c56e8a2817
DxVxDOD <orbangdavid24@gmail.com>
2026-03-28 18:30:16 +0000

    fix: user parser

--- BE_Marks/src/utils/parsers/markParser.ts
+++ BE_Marks/src/utils/parsers/markParser.ts
@@ -2,26 +2,24 @@ import { TMarkFE, TNewMark } from "../../types/mark";
 import { isMarkFromFE, isNewMark } from "../typeGuards/markGuards";
 import { stringParser } from "./generalParsers";
 
-export const newMarkParser = (obj: Partial<TNewMark>) => {
+export const newMarkParser = (obj: Partial<TNewMark>): TNewMark => {
   if (isNewMark(obj)) {
-    const newMark: TNewMark = {
+    return {
       tag: stringParser(obj.tag),
       url: stringParser(obj.url),
       title: stringParser(obj.title),
     };
-    return newMark;
   }
   throw new Error("Failed parsing new Mark");
 };
 
-export const markParser = (obj: Partial<TMarkFE>) => {
+export const markParser = (obj: Partial<TMarkFE>): TMarkFE => {
   if (isMarkFromFE(obj)) {
-    const mark: TMarkFE = {
+    return {
       title: stringParser(obj.title),
       tag: stringParser(obj.tag),
       url: stringParser(obj.url),
     };
-    return mark;
   }
   throw new Error("Failed parsing Mark");
 };
